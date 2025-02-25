ede97e958423e0ebe2f22b04e822b283f479c930
Matheus Pinter <matheuspinter08@gmail.com>
2025-02-25 12:02:46 -0300

    feat: Added the exercise

---
+++ pw3-biscoito/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FrasesComponent } from './frases/frases.component';
+import { ImagemComponent } from './imagem/imagem.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, FrasesComponent, ImagemComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = "pw3-biscoito";
+
+    
+}
---
+++ pw3-biscoito/src/app/imagem/imagem.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imagem',
+  imports: [],
+  templateUrl: './imagem.component.html',
+  styleUrl: './imagem.component.css'
+})
+export class ImagemComponent {
+
+  imagemBiscoito = "biscoito-0.png"
+
+  quebrarBiscoito(): void {
+    this.imagemBiscoito = 'biscoito-1.png';
+  }
+
+  reiniciarBiscoito(): void{
+    this.imagemBiscoito = 'biscoito-0.png'
+  }
+}
